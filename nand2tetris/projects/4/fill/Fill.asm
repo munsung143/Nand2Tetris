6ee963417965ab2c9e87cfb251edff3f728d697d
munsung143 <akunta3475@gmail.com>
2026-01-03 11:55:51 +0900

    [feat] 곱셈, 화면 채우기 어셈블리 코드 작성

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -1,11 +1,46 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/4/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
+(LOOP)
+  @KBD
+  D=M
+  @CLEARSET
+  D;JEQ
+  @R0 //R0 키입력시 -1, 미입력시 0 저장
+  M=-1
+(RETURN)
+  @FILL
+  0;JMP
 
-//// Replace this comment with your code.
+(CLEARSET)
+  @R0
+  M=0
+  @RETURN
+  0;JMP
+
+(FILL)
+  @R1 //R1 루프 카운터
+  M=0
+(FILLLOOP)
+  @R1
+  D=M
+  @8192
+  D=D-A
+  @LOOP
+  D;JEQ
+  @SCREEN
+  D=A
+  @R1
+  D=D+M
+  @R2 //R2 포인팅된 주소 저장
+  M=D
+  @R0
+  D=M
+  @R2
+  A=M
+  M=D
+  @R1
+  M=M+1
+  @FILLLOOP
+  0;JMP
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -1,10 +1,31 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/4/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-// The algorithm is based on repetitive addition.
-
-//// Replace this comment with your code.
+//  n = 0
+// LOOP:
+//  if (n == R1) goto END
+//  n = n + 1
+//  R2 = R2 + R0
+//goto LOOP
+//  END:
+  @n
+  M=0
+  @R2
+  M=0
+(LOOP)
+  @n
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JEQ
+  @n
+  M=M+1
+  @R0
+  D=M
+  @R2
+  M=D+M
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
